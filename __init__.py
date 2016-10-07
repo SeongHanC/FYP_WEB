25f35286854f6a0c6305fc37bbe9a8c02fcab79b
Chuah Seong Han <seonghan94@gmail.com>
2016-10-07 17:58:23 +0800

    Build1
    1. Login Page done
    2. render template issue fixed
    3. navigate to other html file done

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
+from pyld import jsonld
+import json
 
 app = Flask(__name__)
 
@@ -7,5 +9,26 @@ app = Flask(__name__)
 def homepage():
     return render_template('homepage.html')
 
+@app.route('/login',methods=["GET","POST"])
+
+def login():
+
+    error = None
+    if request.method == 'POST':
+
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+
+        else:
+            return redirect(url_for('homepage'))
+
+    return render_template('login.html', error=error)
+
+
+@app.route('/signup')
+
+def signup():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
